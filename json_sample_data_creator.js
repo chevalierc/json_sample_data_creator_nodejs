24d5d8911cda32c1769a24ca7710fc209e76cf76
Christopher Chevalier <chris.e.chevalier@gmail.com>
2016-12-12 11:16:35 -0500

    Update json_sample_data_creator.js

--- json_sample_data_creator.js
+++ json_sample_data_creator.js
@@ -21,15 +21,28 @@ function wiggle_char( char ) {
     }
 }
 
-function wiggle( json ) {
+function wiggle_word(input){
+    var output = ""
+    var input = input + ""
+    for ( var i = 0; i < input.length; i++ ) {
+        var char = input[ i ]
+        var new_char = wiggle_char( char )
+        output += new_char
+    }
+    return output
+}
+
+function wiggle_json( json ) {
     var new_json = {}
+    var regex = new RegExp( '[0-9]{4}-[0-9]{2}-[0-9]{2}.*' )
     for ( var member in json ) {
         if ( json.hasOwnProperty( member ) ) {
-            new_json[ member ] = ""
-            for ( var i = 0; i < json[ member ].length; i++ ) {
-                var char = json[ member ][ i ]
-                var new_char = wiggle_char( char )
-                new_json[ member ] += new_char
+            var is_date = regex.test( json[ member ] )
+            var is_boolean = json[member] === true  || json[member] === false
+            if ( ! (is_date || is_boolean)) {
+                new_json[ member ] = wiggle_word(json[ member ])
+            } else {
+                new_json[ member ] = json[ member ]
             }
         }
     }
@@ -47,12 +60,12 @@ var run = function () {
     var json_count = process.argv[ 3 ]
     var user_given_output = process.argv[ 4 ]
 
-    if(!json_count) json_count = 1
+    if ( !json_count ) json_count = 1
 
     if ( user_given_output ) {
         var folder_name = user_given_output
     } else {
-        var folder_name = "./sampleData_" + json_count + "count"
+        var folder_name = "./sample_" + json_count + "count"
     }
 
     var base_json = JSON.parse( fs.readFileSync( base_json_dir, 'utf8' ) )
@@ -62,14 +75,14 @@ var run = function () {
         var og_folder_name = folder_name
         while ( fs.existsSync( folder_name ) ) {
             i++
-            folder_name = og_folder_name + "_version" + i
+            folder_name = og_folder_name + "_v" + i
         }
     }
     fs.mkdirSync( folder_name )
 
     for ( var i = 1; i <= json_count; i++ ) {
         var cur_json = _.clone( base_json )
-        var new_json = wiggle( cur_json )
+        var new_json = wiggle_json( cur_json )
         write( new_json, folder_name + "/json_" + i + ".json" )
     }
 
